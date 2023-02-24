a2ad3b131001e3298a9d93b497f7bba3c3d06cb9
TKPhuong <phuong.to@ips.ne.jp>
2023-02-24 18:34:14 +0900

    change test_check_disk to use parametrize

--- src/templates/utils/helper_funcs/check_disk.py
+++ src/templates/utils/helper_funcs/check_disk.py
@@ -1,21 +1,31 @@
 import shutil
 
+def check_disk_space(folder: str, attention_size: float, ok_size: float, unit: str = "GB") -> str:
+    """
+    指定されたフォルダーのディスクスペースをチェックして、空き容量に応じたステータスを返します。
 
-def check_disk_space(folder: str, attention_size: int, ok_size: int):
+    :param folder: ディスクスペースをチェックするフォルダーのパス。
+    :param attention_size: 警告を発生させるしきい値。この値よりも大きい場合、ステータスは OK となります。
+    :param ok_size: クリティカルなアラートを発生させるしきい値。この値よりも大きい場合、ステータスは WARN となります。
+    :param unit: 空き容量チェックに使用する単位。これは "GB"（ギガバイト）、"MB"（メガバイト）、または "KB"（キロバイト）のいずれかで指定できます。デフォルトは "GB" です。
+
+    :return: ディスクスペースの空き容量に基づくステータスを示す文字列。OK、WARN、NG のいずれか。
     """
-    ディスクスペースの容量をチェックする。
-    :Parameters:
-        :folder: ディスクスペースをチェックするフォルダーのパス
-        :attention_size: 警告を出すしきい値。この値よりも大きい場合、ステータスは OK となる。
-        :ok_size: 正常な状態とするしきい値。この値よりも大きい場合、ステータスは WARN となる。
-    :Returns:
-        :status: ディスクスペースの容量に基づくステータスを返す。OK, WARN, NG のいずれか。
-    """
-    # ディスクの容量をGBに変換
+    # ユニット名と変換係数を対応付ける辞書を定義
+    conversion_factors = {
+        "GB": 2 ** 30,
+        "MB": 2 ** 20,
+        "KB": 2 ** 10,
+    }
+
+    # 指定されたユニット名に対応する変換係数を取得し、存在しない場合は "GB" を使用する
+    conversion_factor = conversion_factors.get(unit, conversion_factors["GB"])
+
+    # ディスクスペースを指定された単位に変換する
     total, used, free = shutil.disk_usage(folder)
-    free_disk = free / (2 ** 30)
+    free_disk = free / conversion_factor
 
-    # ステータスをチェック
+    # ステータスをチェックする
     if free_disk > attention_size:
         status = "OK"
     elif free_disk > ok_size:
@@ -24,4 +34,3 @@ def check_disk_space(folder: str, attention_size: int, ok_size: int):
         status = "NG"
 
     return status
-
--- src/tests/test_check_disk.py
+++ src/tests/test_check_disk.py
@@ -1,2 +1,37 @@
-def test_check_disk():
-    assert True
+import shutil
+import os
+import sys
+import pytest
+import tempfile
+import shutil
+
+# このファイルの親ディレクトリをシステムパスに追加する
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+from templates.utils.helper_funcs.check_disk import check_disk_space
+
+
+@pytest.mark.parametrize("unit, ok_size_delta, attention_size_delta, write_size, expected_status", [
+    ("MB", -7, -5, 6, "WARN"),       # WARN status test case
+    ("MB", -10, -7, 6, "OK"),          # OK status test case
+    ("MB", -5, -3, 6, "NG"),        # NG status test case
+    ("KB", -800, -500, 300, "OK"),       # OK status test case (KB)
+    ("GB", -0.3, -0.2, 0.1, "OK"),       # OK status test case (GB)
+])
+def test_check_disk_space(unit, ok_size_delta, attention_size_delta, write_size, expected_status):
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # 空き容量が極端に不十分な一時ディレクトリを作成し、そのディスク容量をチェックする
+        conv_factor = {"GB": 2 ** 30,"MB": 2 ** 20,"KB": 2 ** 10}.get(unit)
+        temp_file = os.path.join(temp_dir, "temp.txt")
+        free_size = shutil.disk_usage(temp_dir).free / conv_factor
+        with open(temp_file, "wb") as f:
+            f.write(os.urandom(int(write_size*conv_factor)))  # サイズwrite_size(unit)のランダムファイルを作成
+        ok_size = free_size + ok_size_delta
+        attention_size = free_size + attention_size_delta
+        status = check_disk_space(temp_dir, attention_size, ok_size, unit)
+
+        # ステータスが "NG" であることを検証する
+        assert status == expected_status
+
