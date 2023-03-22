c719593ada3fce4582a5d58064c61c8f54773ac4
TKPhuong <phuong.to@ips.ne.jp>
2023-03-22 16:48:11 +0900

    edit TimerThread

--- src/templates/threadman/timer_thread.py
+++ src/templates/threadman/timer_thread.py
@@ -1,12 +1,12 @@
 # Timer
-from time import time
+from time import time, sleep
 from threading import Timer
 import datetime
 from typing import Optional
 
 
 
-class ThreadTimer():
+class TimerThread():
     """
     スレッドタイマークラス。
     ----
@@ -26,7 +26,7 @@ class ThreadTimer():
         self.kwargs = kwargs
 
     def start(self, start_time:Optional[time] = None):
-        self.start_time = start_time or self._time2datetime(time())
+        self.start_time = start_time or time()
         self.thread = Timer(self.interval, self.handle_function,
                             args=self.args, kwargs=self.kwargs)
         self.thread.start()
@@ -36,28 +36,30 @@ class ThreadTimer():
             self.thread.cancel()
         self.start_time = 0
 
-    @staticmethod
-    def _time2datetime(now):
-        """
-        現在の日付と時刻を取得するための関数
-        :Parameters:
-            :now: float型。time.time()の戻り値
-        :Returns:
-            :now_ymdhms: 現在の年月日時分秒
-        """
-        # 現在の年、月、日、時、分、秒
-        now_ymdhms = datetime.datetime.fromtimestamp(now)
-        now_ymdhms = now_ymdhms.strftime("%Y年%m月%d日%H時%M分%S秒")
-        return now_ymdhms
+    # @staticmethod
+    # def _time2datetime(now):
+    #     """
+    #     現在の日付と時刻を取得するための関数
+    #     :Parameters:
+    #         :now: float型。time.time()の戻り値
+    #     :Returns:
+    #         :now_ymdhms: 現在の年月日時分秒
+    #     """
+    #     # 現在の年、月、日、時、分、秒
+    #     now_ymdhms = datetime.datetime.fromtimestamp(now)
+    #     now_ymdhms = now_ymdhms.strftime("%Y年%m月%d日%H時%M分%S秒")
+    #     return now_ymdhms
 
 
 if __name__ == "__main__":
-    def func(start_time):
-        print(f"handle passed to init executed, time:{start_time}")
-    t = ThreadTimer(1, func, start_time=time())
+    def func(name):
+        start_time = time()
+        print(f"handle {name} passed to init executed, time:{start_time}")
+    t = TimerThread(1, func, name="func1")
     now = time()
     t.cancel()
     # print(now)
     t.start()
+    sleep(2)
     t.start()
     # sleep(5)
